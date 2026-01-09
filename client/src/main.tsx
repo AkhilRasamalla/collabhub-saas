7988b1f77f3d8597fcb732f7e926cac4c24caa49
Akhil Rasamalla <akhilrasamalla0805@gmail.com>
2026-01-09 13:32:37 +0530

    fix: wrap AuthProvider and Router correctly

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,22 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { NuqsAdapter } from "nuqs/adapters/react";
 
 import "./index.css";
-import App from "./App.tsx";
-import QueryProvider from "./context/query-provider.tsx";
-import { Toaster } from "./components/ui/toaster.tsx";
+import App from "./App";
+import QueryProvider from "./context/query-provider";
 import { AuthProvider } from "./context/auth-provider";
+import { Toaster } from "./components/ui/toaster";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryProvider>
-      <AuthProvider>
-        <NuqsAdapter>
-          <App />
-        </NuqsAdapter>
-        <Toaster />
-      </AuthProvider>
-    </QueryProvider>
+    <BrowserRouter>
+      <QueryProvider>
+        <AuthProvider>
+          <NuqsAdapter>
+            <App />
+          </NuqsAdapter>
+          <Toaster />
+        </AuthProvider>
+      </QueryProvider>
+    </BrowserRouter>
   </StrictMode>
 );
